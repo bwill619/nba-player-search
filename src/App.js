818cfde159bce332baad01bf401b9b2d0822a3a9
bwill619 <bjw0619@gmail.com>
2021-10-27 21:46:08 -0400

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      players:[]
+    }
+    this.searchFunction = this.searchFunction.bind(this);
+  }
+
+  searchFunction() {
+    let input = document.getElementsByClassName('textBar').value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('testing');
+
+  }
+
+  async componentDidMount() {
+    const url = 'https://www.balldontlie.io/api/v1/players?search=lebron'
+    let result = null;
+    try {
+      result = await axios(url, {
+        headers: {
+          Accept: "application/json"
+        }
+      })
+    } catch(e) {
+      console.log(e)
+    }
+    this.setState({players : result.data.data})
+  }
+
+
+  render(){
+    const{players} = this.state;
+    console.log({players})
+    let mappedArray = (players).map((players) => {
+      return(
+        <table class="playerTable">
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Height</th>
+            <th>Weight</th>
+            <th>Team</th>
+          </tr>
+          <tr>
+            <td>{players.first_name}</td>
+            <td>{players.last_name}</td>
+            <td>{players.height_feet}'{players.height_inches}</td>
+            <td>{players.weight_pounds}</td>
+            <td>{players.team.full_name}</td>
+          </tr>
+        </table>
+      )
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='nbaTitle'>NBA Player Search</h1>
+      <body class="appBody">
+        <form>
+          <label for="search" class="searchLabel">Search For Player:</label>
+          <input type="text" class="textBar" name ="search"></input>
+          <input type="button" class="playerButton" onclick = "searchFunction()" value="Search"></input>
+        </form>
+        <ul className="tessting">
+          {mappedArray}
+          </ul>
+      </body>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/main.js
@@ -0,0 +1,1 @@
+import nba from "../src/nba";
