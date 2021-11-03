ae8c0004e700e28fe17a9639efe6859018b2781a
bwill619 <bjw0619@gmail.com>
2021-11-02 23:56:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      players:[]
+      players:[],
     }
     this.searchFunction = this.searchFunction.bind(this);
   }
